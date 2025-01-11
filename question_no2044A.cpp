0769127df3dbd47e2b15f91c12abe6b9e5da1045
Rishabh Sharma <rishabhshrama8734@gmail.com>
2025-01-11 13:14:11 +0530

    first commit

--- question_no2044A.cpp
+++ question_no2044A.cpp
@@ -1,9 +1,59 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+
+//     int t;
+//     cin >> t;  // Read the number of test cases
+    
+//     while (t--) {
+//         int n;
+//         cin >> n;  // Read the value of n for each test case
+        
+//         // The number of valid pairs (a, b) is simply n - 1
+//         cout << n - 1 << endl;
+//     }
+    
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;  // Read the number of test cases
+    
+//     // Process each test case
+//     for (int i = 0; i < t; i++) {
+//         int n;
+//         cin >> n;  // Read the value of n for this test case
+        
+//         // The number of valid pairs is simply n - 1
+//         cout << n - 1 << endl;
+//     }
+    
+//     return 0;
+// }
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int count_pairs(int n) {
+    return (n - 1) / 2 + 1;
+}
 
-    cout<<"hi print this Rishbah";
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << count_pairs(n) << endl;
+    }
+
     return 0;
 }
+
+
