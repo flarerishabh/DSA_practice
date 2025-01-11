a84fa067852394f96d6426512a343f2ecbced46e
Rishabh Sharma <137152843+flarerishabh@users.noreply.github.com>
2025-01-11 13:18:00 +0530

    Update question_no2044A.cpp

--- question_no2044A.cpp
+++ question_no2044A.cpp
@@ -1,40 +1,4 @@
-// #include <bits/stdc++.h>
-// using namespace std;
 
-// int main(){
-
-
-//     int t;
-//     cin >> t;  // Read the number of test cases
-    
-//     while (t--) {
-//         int n;
-//         cin >> n;  // Read the value of n for each test case
-        
-//         // The number of valid pairs (a, b) is simply n - 1
-//         cout << n - 1 << endl;
-//     }
-    
-//     return 0;
-// }
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int t;
-//     cin >> t;  // Read the number of test cases
-    
-//     // Process each test case
-//     for (int i = 0; i < t; i++) {
-//         int n;
-//         cin >> n;  // Read the value of n for this test case
-        
-//         // The number of valid pairs is simply n - 1
-//         cout << n - 1 << endl;
-//     }
-    
-//     return 0;
-// }
 #include <iostream>
 
 using namespace std;
